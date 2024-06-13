05acc306d1b5a94dcdca52ba0f9b20635c6bffaa
abdielsouza <abdielcsouza@gmail.com>
2024-06-13 14:20:13 -0300

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 
 export const Navbar = () => {
     return (
-        <nav className="fixed bg-black z-10 text-white px-4 sm:scale-75 md:scale-100 md:flex md:justify-between md:items-center w-full m-0 top-0">
+        <nav className="fixed bg-black z-10 text-white sm:px-1 md:px-4 sm:scale-95 md:scale-100 md:flex md:justify-between md:items-center w-full m-0">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold">Code Zero</h1>
                 <div>
